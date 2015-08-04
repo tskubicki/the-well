a74bc0650a5af38e04bd5ae088a61a2b4667e57e
Thomas Skubicki <tskubicki@gmail.com>
2015-08-04 15:57:48 -0400

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,45 @@
 class UsersController < ApplicationController
-  def index
-  end
+	before_action :authenticate_user!
+	def index
+		redirect_to root_path
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			#success
+		else
+			#failure
+		end
+	end
+	
+	def new
+		@user = User.new()
+	end
+
+	def edit
+		@user = current_user
+	end
+
+	def show
+		@user = current_user
+	end
+
+	def update
+		@user = current_user
+		@user.update(user_params)
+		if @user.save
+			#success
+		else
+			#failure
+		end
+	end
+
+	def destroy
+	end
+	
+	private
+	def user_params
+		params.require(:user).permit('stuff') #to be added. -TS
+	end
 end
--- app/controllers/waiters_controller.rb
+++ app/controllers/waiters_controller.rb
@@ -1,4 +1,23 @@
 class WaitersController < ApplicationController
+	before_action :authenticate_waiter!
   def index
   end
+  
+  def new
+	@order = Order.new()
+  end
+  
+  def edit
+	
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
 end
