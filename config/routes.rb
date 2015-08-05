b928a640846d86d3579e17d3afd1782a591aaa66
Thomas Skubicki <tskubicki@gmail.com>
2015-08-05 11:34:03 -0400

    added a home page with its controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'orderitems/index'
 
   get 'items/index'
@@ -33,7 +35,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
